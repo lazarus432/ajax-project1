76826134d370a1d12ca26e8f7e539ae01e9e92a9
Nic <deux02030@gmail.com>
2017-08-28 07:31:48 -0400

    added ny times ajax request

--- js/script.js
+++ js/script.js
@@ -22,6 +22,20 @@ function loadData() {
     var streetviewURL = 'http://maps.googleapis.com/maps/api/streetview?size=600x400&location=' + address + '';
     $body.append('<img class = "bgimg" src = "' + streetviewURL + '">');
 
+    // ny times ajax request
+    var nytimesURL = 'http://api.nytimes.com/svc/search/v2/articlesearch.json?q=' + cityStr + '&sort=newest&api-key=f2fe46a5bb804f7897e5b48c502f80c3'
+    $.getJSON(nytimesURL, function(data){
+      $nytHeaderElem.text('New York Times Articles About ' + cityStr);
+
+      articles = data.response.docs;
+      for (var i = 0; i < articles.length; i++) {
+        var article = articles[i];
+        $nytElem.append('<li class = "article">' +
+          '<a href ="' + article.web_url + '">' + ' ' + article.headline.main + '</a>'
+          + '<p>' + article.snippet + '</p>' + '</li>');
+      };
+    })
+
     return false;
 };
 
